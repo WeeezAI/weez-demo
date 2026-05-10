548fbc782e86323b0fe0cdc0afb3bf8d611c8cb7
Shakil <sayyadshakilgdsc@gmail.com>
2026-05-11 02:37:09 +0530

    Fixed the Reference Error

--- src/pages/AutonomousMarketing.tsx
+++ src/pages/AutonomousMarketing.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef, KeyboardEvent, ChangeEvent } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, useSearchParams } from "react-router-dom";
 import {
     ArrowUp,
     BrainCircuit,
